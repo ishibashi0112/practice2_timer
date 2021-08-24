95a33160a09f163677d42213ff52055d3a69b38c
ishibashi0112 <65233660+ishibashi0112@users.noreply.github.com>
2021-08-24 23:13:48 +0900

    initial_commit
    制作途中

---
+++ timer.js
@@ -0,0 +1,14 @@
+const hour = document.getElementById("hour");
+const mintues =  document.getElementById("mintues");
+const seconds = document.getElementById("seconds");
+
+const start = document.getElementById("start_button");
+const stop = document.getElementById("stop_button");
+
+start.addEventListener("click",function(){
+  console.log(hour)
+});
+
+stop.addEventListener("click",function(){
+  console.log(hour)
+});
