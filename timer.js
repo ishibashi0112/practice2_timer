de7d45ef02754c1803727b7248f7e559da457750
ishibashi0112 <65233660+ishibashi0112@users.noreply.github.com>
2021-08-25 23:30:04 +0900

    2nd_commit
    途中経過

--- timer.js
+++ timer.js
@@ -1,14 +1,36 @@
 const hour = document.getElementById("hour");
 const mintues =  document.getElementById("mintues");
 const seconds = document.getElementById("seconds");
-
 const start = document.getElementById("start_button");
 const stop = document.getElementById("stop_button");
 
+// function addHTML(){
+
+//   const HTML = 
+// }
+const timer = document.getElementById("changeTimer")
+const children =  timer.children ;
+console.log(children)
+function deleteInput(){
+  const timer = document.getElementById("changeTimer")
+  while (timer.firstChild) {
+    timer.removeChild(timer.firstChild)
+  }
+  // const children =  timer.children ;
+  // console.log(children)
+};
+
 start.addEventListener("click",function(){
-  console.log(hour)
+  const hourValue = hour.value
+  const mintuesValue = mintues.value
+  const secondsValue = seconds.value
+  console.log(hourValue,mintuesValue)
+  
+  deleteInput()
+  console.log(children)
 });
 
 stop.addEventListener("click",function(){
-  console.log(hour)
+  console.log();
+  
 });
